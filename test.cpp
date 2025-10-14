30166f263b92ab70ee039af85826eb590e3cbf56
Noran <noranelhalwagy515@gmail.com>
2025-10-14 23:32:50 +0300

    Initial commit

---
+++ test.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long;
+#define el "\n"
+#define loop(n) for(int i=0; i<n; i++)
+#define sz(x) (int)(x).size()
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define Cin(s)  for(auto&x:s)cin>>x
+#define FIO ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+int main() {
+FIO;
+ int n;
+ int s=0;
+ 
+
+
+    return 0;
+}
